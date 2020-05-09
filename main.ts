2a8cc202d7f0a85433e710c5c88ed67b2798f075
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-05-09 18:06:33 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,80 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # # # # #
+        # . . . #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # .
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # .
+        # . . . .
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # .
+        # . . . .
+        # . . . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . . # #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showString("A")
+    basic.pause(2000)
+    basic.showString("B")
+    basic.pause(2000)
+    basic.showString("C")
+    basic.pause(2000)
+})
